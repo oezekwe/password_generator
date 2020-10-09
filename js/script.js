5bbbd2fab51e75cf8883c103f2ce9cf0dfe7acae
Obinna Ezekwe <ozekwe@gmail.com>
2020-10-09 07:27:37 -0500

    Added confirm and prompts

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 // Assignment code here
+var chkUpper, chkLower, chkNumeric, chkSpecial;
 
+function generatePassword(){
+  let charsLen= prompt("Choose a length that at least 8 but no more than 128 characters");
+  chkUpper= confirm("Do you want it uppercase?");
+  chkLower= confirm("Do you want it lowercase?");
+  chkNumeric= confirm("Do you want it numeric?");
+  chkSpecial= confirm("Do you want it special?");
+
+  
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+
 
 // Write password to the #password input
 function writePassword() {
