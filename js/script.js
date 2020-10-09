c461f92a1a77c9be5e2ae41573a97a3f303f5062
Obinna Ezekwe <ozekwe@gmail.com>
2020-10-09 08:47:41 -0500

    Created loop and conditional statements

--- js/script.js
+++ js/script.js
@@ -1,14 +1,38 @@
 // Assignment code here
 var chkUpper, chkLower, chkNumeric, chkSpecial;
+var randNum;
 
 function generatePassword(){
-  let charsLen= prompt("Choose a length that at least 8 but no more than 128 characters");
+  var dec, flagR;
+  var charsLen= prompt("Choose a length that at least 8 but no more than 128 characters");
   chkUpper= confirm("Do you want it uppercase?");
   chkLower= confirm("Do you want it lowercase?");
   chkNumeric= confirm("Do you want it numeric?");
   chkSpecial= confirm("Do you want it special?");
 
-  
+  for(let c=0; c<charsLen; c++){
+    flagR= true;
+    dec= Math.floor(Math.random()*126)+32;
+    while(flagR){
+      if(chkUpper && (dec >= 65 && dec <= 90)){
+        flagR= false;
+      }
+      else if(chkLower && (dec >= 97 && dec <= 122)){
+        flagR= false;
+      }
+      else if(chkNumeric && (dec >= 48 && dec <= 57)){
+        flagR= false;
+      }
+      else if(chkSpecial && (dec >= 32 && dec <= 47) || (dec >= 58 && dec <= 64)
+      (dec >= 91 && dec <= 96) || (dec >= 123 && dec <= 126)){
+        flagR= false;
+      }
+      else{
+        dec= Math.floor(Math.random()*126)+32;
+      }
+    }
+    
+  }
 }
 
 // Get references to the #generate element
