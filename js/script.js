ce353ac15cee17197f11f10fef27d246bc61cbe0
Obinna Ezekwe <ozekwe@gmail.com>
2020-10-10 00:14:24 -0500

    Apply selected criterias for a password

--- js/script.js
+++ js/script.js
@@ -1,18 +1,77 @@
 // Assignment code here
-var chkUpper, chkLower, chkNumeric, chkSpecial;
+var chkUpper, chkLower, chkNumeric, chkSpecial, selectCriteria;
+
+function choosingCriteria(){
+  var num;
+  var Word= "";
+  selectCriteria=0;
+  do{
+    chkUpper= confirm("Do you want it uppercase?");
+    if(chkUpper){
+      selectCriteria++;
+      num= Math.floor(Math.random()*126)+1;
+      while(!(num >= 65 && num <= 90)){
+        num= Math.floor(Math.random()*126)+1;
+      }
+      Word= Word+String.fromCharCode(num);
+    }
+  
+    chkLower= confirm("Do you want it lowercase?");
+    if(chkLower){
+      selectCriteria++;
+      num= Math.floor(Math.random()*126)+1;
+      while(!(num >= 97 && num <= 122)){
+        num= Math.floor(Math.random()*126)+1;
+      }
+      Word= Word+String.fromCharCode(num);
+    }
+  
+    chkNumeric= confirm("Do you want it numeric?");
+    if(chkNumeric){
+      selectCriteria++;
+      num= Math.floor(Math.random()*126)+1;
+      while(!(num >= 48 && num <= 57)){
+        num= Math.floor(Math.random()*126)+1;
+      }
+      Word= Word+String.fromCharCode(num);
+    }
+    
+    chkSpecial= confirm("Do you want it special?");
+    if(chkSpecial){
+      selectCriteria++;
+      num= Math.floor(Math.random()*126)+1;
+      debugger;
+      while((!(num >= 32 && num <= 47)) && (!(num >= 58 && num <= 64)) 
+      && (!(num >= 91 && num <= 96)) && (!(num >= 123 && num <= 126))){
+        num= Math.floor(Math.random()*126)+1;
+      }
+      Word= Word+String.fromCharCode(num);
+    } 
+  } while(selectCriteria==0);
+
+  return Word;
+}
 
 function generatePassword(){
   var dec, flagR, singleC;
   var pWord= "";
   var charsLen= prompt("Choose a length that at least 8 but no more than 128 characters");
-  chkUpper= confirm("Do you want it uppercase?");
-  chkLower= confirm("Do you want it lowercase?");
-  chkNumeric= confirm("Do you want it numeric?");
-  chkSpecial= confirm("Do you want it special?");
-  debugger;
+  while((charsLen < 8) || (charsLen > 128)){
+    if(charsLen < 8){
+      alert("Not enough characters. Please try again.");
+      charsLen= prompt("Choose a length that at least 8 but no more than 128 characters");
+    }
+    else{
+      alert("Too many characters. Please try again.");
+      charsLen= prompt("Choose a length that at least 8 but no more than 128 characters");
+    }
+  }
+  pWord= choosingCriteria();
+  charsLen-= selectCriteria;
+  
   for(let c=0; c<charsLen; c++){
     flagR= true;
-    dec= Math.floor(Math.random()*126)+32;
+    dec= Math.floor(Math.random()*126)+1;
     while(flagR){
       if(chkUpper && (dec >= 65 && dec <= 90)){
         flagR= false;
